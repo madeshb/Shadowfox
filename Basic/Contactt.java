540e63b64fc9ca4975711d2a8b70f743d9e8ba49
Madesh B <146665900+madeshb@users.noreply.github.com>
2025-03-01 18:39:28 +0530

    Create Contactt.java

---
+++ Basic/Contactt.java
@@ -0,0 +1,45 @@
+package com.madesh.demo;
+
+public class Contactt {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    // Constructor
+    public Contactt(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nPhone: " + phoneNumber + "\nEmail: " + email;
+    }
+}
+
